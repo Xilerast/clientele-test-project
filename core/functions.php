d8392e6008831254ebcb5df059cddba3386c7eb1
Xilerast <laertisD9@gmail.com>
2024-04-22 10:40:51 +0300

    Fixed deprecation

--- core/functions.php
+++ core/functions.php
@@ -1,7 +1,7 @@
 <?php
 
     function findMaxId($database, $table) {
-        $searchStatement = $database->getPdo()->prepare("select MAX(id) as id from ${table}");
+        $searchStatement = $database->getPdo()->prepare("select MAX(id) as id from {$table}");
         $searchStatement->execute();
         $maxIdResult = $searchStatement->fetchAll(PDO::FETCH_OBJ);
         return $maxIdResult[0]->id;
